5fa445dec0f844834c8b0cacbe76915d9ed7ed5e
Eric Typaldos <ettypaldos@gmail.com>
2017-02-03 22:37:20 -0500

    Working

--- scrape.py
+++ scrape.py
@@ -1,11 +1,16 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from time import sleep
 
-chromedriver = "/Users/Joel/Documents/Hacks/instaScrape/chromedriver"
-os.environ['webdriver.chrome.driver'] = chromedriver
-driver = webdriver.Chrome(chromedriver)
+# chromedriver = "/Users/eric/instascrape/chromedriver"
+driver = webdriver.Chrome()
+# os.environ['webdriver.chrome.driver'] = chromedriver
+# driver = webdriver.Chrome(chromedriver)
 
 driver.get("https://instagram.com")
 
@@ -29,3 +34,53 @@ searchElement.send_keys('#cafeSG')
 sleep(3)
 
 driver.get("https://instagram.com/explore/tags/cafesg/")
+
+# Save the window opener (current window, do not mistaken with tab... not the same)
+main_window = driver.current_window_handle
+
+# Force to infinite scroll mode (click "Load more")
+driver.find_element_by_link_text('Load more').click()
+
+def whatever(last_link):
+    sleep(0.5)
+    current_pointer = 0
+    # Go to profile page
+    recentPhotoElements = driver.find_elements_by_css_selector('._myci9 > a')
+    for link in recentPhotoElements:
+        current_pointer += 1
+        # print(link)
+        if current_pointer > last_link:
+            # Open the link in a new tab by sending key strokes on the element
+            # Use: Keys.CONTROL + Keys.SHIFT + Keys.RETURN to open tab on top of the stack
+            sleep(0.3)
+            link.send_keys(Keys.COMMAND + Keys.SHIFT + Keys.RETURN)
+            sleep(0.3)
+
+            # Put focus on current window which will, in fact, put focus on the current visible tab
+            driver.switch_to.window(driver.window_handles[1])
+
+            # Click user profile link
+            driver.find_element_by_css_selector('._f95g7 > a').click()
+            try:
+                bio = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "._bugdy > span"))
+                )
+                # Get inner text
+                inner_text = driver.execute_script("return arguments[0].innerText;", bio)
+                print(inner_text)
+            except TimeoutException:
+                print("No bio")
+
+            # Close current tab
+            # driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'w')
+            driver.close()
+            driver.switch_to.window(driver.window_handles[-1])
+
+            # Put focus on current window which will be the window opener
+            driver.switch_to_window(main_window)
+
+
+    # Call self
+    whatever(current_pointer)
+
+whatever(0)
